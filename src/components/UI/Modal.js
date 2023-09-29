ba40de13902a4e5b7a6ff3c49e8bf63f45d3107a
Burak Yigitaslan <10brk506@gmail.com>
2023-09-29 22:41:44 +0300

    completed

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
